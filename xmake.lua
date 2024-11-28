caf000c52fbda081ee8c0146c70bcd0d2e5a5222
Bowen XU <58252898+bowen-xu@users.noreply.github.com>
2024-11-28 11:07:43 -0500

    config xmake installation

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,23 @@
 add_rules("mode.debug", "mode.release", "mode.releasedbg")
 set_languages("cxx20")
 
--- set_targetdir(".")
+set_version("1.0.0")
+
+target("randomcpp")
+    set_kind("headeronly")
+    add_includedirs("./include")
+
+    on_install(function (package)
+        local install_dir = package:installdir()
+    
+        local header_src_dir = path.join(package:scriptdir(), "include")  -- 假设头文件存放在 src 目录
+        local header_dest_dir = path.join(install_dir, "include")    -- 目标路径是安装目录下的 include 文件夹
+    
+        os.mkdir(header_dest_dir)
+    
+        os.cp(path.join(header_src_dir, "*.h"), header_dest_dir)
+    end)
+
 
 add_requires("gtest")
 target("randomcpp_test")
