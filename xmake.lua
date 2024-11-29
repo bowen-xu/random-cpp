36a214410f5e2d6b723f190d8f6ceec1a3a3fa42
Bowen XU <58252898+bowen-xu@users.noreply.github.com>
2024-11-29 00:35:11 -0500

    update

--- xmake.lua
+++ xmake.lua
@@ -1,24 +1,41 @@
-add_rules("mode.debug", "mode.release", "mode.releasedbg")
-set_languages("cxx20")
+package("randomcpp")
+    set_description("C++ library for random number generation with Pythin-like APIs")
 
-set_version("1.0.0")
-
-target("randomcpp")
-    set_kind("headeronly")
-    add_includedirs("./include")
+    set_homepage("https://github.com/bowen-xu/random-cpp")
+    add_urls("https://github.com/bowen-xu/random-cpp.git")
+    set_license("MIT")
+    set_kind("library")
+    -- on_install("macosx", "linux", function (package)
+    --     import("package.tools.autoconf").install(package)
+    -- end)
 
     on_install(function (package)
-        local install_dir = package:installdir()
-    
-        local header_src_dir = path.join(package:scriptdir(), "include")  -- 假设头文件存放在 src 目录
-        local header_dest_dir = path.join(install_dir, "include")    -- 目标路径是安装目录下的 include 文件夹
-    
-        os.mkdir(header_dest_dir)
-    
-        os.cp(path.join(header_src_dir, "*.h"), header_dest_dir)
+        os.cp("include", package:installdir())
     end)
 
+package_end()
 
+-- add_rules("mode.debug", "mode.release", "mode.releasedbg")
+-- set_languages("cxx20")
+
+-- set_version("1.0.0")
+
+-- target("randomcpp")
+--     set_kind("headeronly")
+--     add_includedirs("./include")
+
+--     -- on_install(function (package)
+--     --     local install_dir = package:installdir()
+    
+--     --     local header_src_dir = path.join(package:scriptdir(), "include") 
+--     --     local header_dest_dir = path.join(install_dir, "include")
+    
+--     --     os.mkdir(header_dest_dir)
+    
+--     --     os.cp(path.join(header_src_dir, "*.h"), header_dest_dir)
+--     -- end)
+
+add_requires("randomcpp")
 add_requires("gtest")
 target("randomcpp_test")
     set_kind("binary")
